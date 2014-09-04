6fd6ea4f3e1d9546aadc79b91917fe59c91520f8
Veiko Sinivee <veiko.sinivee@gmail.com>
2014-09-04 14:56:07 +0300

    #23671 packaging fixes rebased

--- packaging/build-ndigidoc.rb
+++ packaging/build-ndigidoc.rb
@@ -13,7 +13,7 @@ msi_name = "Eesti_ID_kaart-NET-teek-arendajale-#{version}#{version_suffix}.msi"
 #prepares source for packaging
 FileUtils.rm_rf "#{source_location}\\NDigiDoc"
 FileUtils.mkdir_p "#{source_location}"
-FileUtils.cp_r "..", source_location
+FileUtils.cp_r "../../NDigiDoc/", "#{source_location}"
 FileUtils.rm_rf "#{source_location}\\NDigiDoc\\NDigiDoc\\obj"
 FileUtils.rm_rf "#{source_location}\\NDigiDoc\\NDigiDocUtility\\obj"
 
@@ -24,8 +24,7 @@ execute_shell_command "#{visual_studio_cmd("x64")} && #{heat} dir #{source_locat
 create_build_directory
 
 FileUtils.cd build_directory do
-#  FileUtils.cp_r "../../../../NDigiDoc/.", "."
-  FileUtils.cp "./packaging/ndigidoc-dev.wxs", "."
+  FileUtils.cd "../.."
 
   execute_shell_command "#{visual_studio_cmd("x64")} && msbuild NDigiDoc.sln /p:Configuration=Release /p:Platform=\"Any CPU\""
 
@@ -40,11 +39,13 @@ FileUtils.cd build_directory do
   sign_files_win "\\install\\IdCard\\ndigidoc\\NDigiDocUtility.exe"
   FileUtils.cp "NDigiDocUtility/bin/Release/log4n.xml", "/install/IdCard/ndigidoc"
 
+  FileUtils.cp "#{source_location}/NDigiDoc/packaging/ndigidoc-dev.wxs", "."
   execute_shell_command "#{visual_studio_cmd("x64")} && #{candle} ndigidoc-dev.wxs SourceFilesFragment.wxs -v -ext WixIIsExtension"
   execute_shell_command "#{visual_studio_cmd("x64")} && #{light} -out #{msi_name} ndigidoc-dev.wixobj SourceFilesFragment.wixobj -v -ext WixIIsExtension"
 
   sign_files_win msi_name
 
   win_put_other_package_to_repository File.basename(msi_name, File.extname(msi_name)),"", "Eesti_ID_kaart-NET-teek-arendajale-*"
+  FileUtils.rm_rf "#{source_location}\\NDigiDoc\\NDigiDoc\\packaging"
 
 end
