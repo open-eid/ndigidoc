a73d972f13c367836c427f8ccbaac6ee89ecd5ab
Veiko Sinivee <veiko.sinivee@gmail.com>
2015-03-06 10:42:58 +0200

    #IB-3525 ndigidoc jenkins build fix

--- packaging/build-ndigidoc.bat
+++ packaging/build-ndigidoc.bat
@@ -1,18 +1,24 @@
 rem --------------------------------------------
 rem build-ndigidoc.bat
+rem BEFORE USING THIS SCIPT 
+rem define in the environment:
+rem set BUILD_NUMBER=<some-build-number>
+rem set VC_DIR=<path-to-your-visual-studio>. Check that the provided location is correct.
+rem set PROJECT_DIR=<project-dir>. Where NDigiDoc.sln is
+rem set INSTALL_DIR=<output-directory>. Schould be c:\install\IdCard\ndigidoc
+rem because VC project outputs files there. This dir is used later for packaging.
+rem download CLR security Security.Cryptography.dll 1.6 from http://clrsecurity.codeplex.com/downloads/get/128947
+rem and unpack it to %INSTALL_DIR%\clrsecurity or directly to %PROJECT_DIR%\clrsecurity
 rem --------------------------------------------
 
 set build_number = %BUILD_NUMBER%
 set repository = %EXTERNAL_REPOSITORY%
-set INSTALL_DIR=c:\install
+echo INSTALL DIR: %INSTALL_DIR%
 SET INSTALL_BIN_DIR=%INSTALL_DIR%\IdCard\ndigidoc
 SET INSTALL_SRC_DIR=%INSTALL_DIR%\source\ndigidoc
-SET VC_DIR=C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC
+SET VC_DIR=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC
 SET VC_VARS_CMD="%VC_DIR%\vcvarsall.bat"
-SET GIT_SRC_DIR=c:\jenkins\workspace\nDigiDoc\idkaart\current\ndigidoc
-SET GIT_BUILD_DIR=c:\jenkins\workspace\nDigiDoc\label\Windows_trunk_VS2012\idkaart\current\ndigidoc
-set PREFIX=\install
-set sourceLocation=\install\source\ndigidoc
+echo PROJECT DIR: %PROJECT_DIR%
 
 echo "Cleanup install bin dir"
 rmdir /S /Q %INSTALL_BIN_DIR%
@@ -23,7 +29,7 @@ rmdir /S /Q %INSTALL_SRC_DIR%
 mkdir %INSTALL_SRC_DIR%
 
 echo "Copying sources to install src dir"
-xcopy /Y /E %GIT_SRC_DIR% %INSTALL_SRC_DIR%
+xcopy /Y /E %PROJECT_DIR% %INSTALL_SRC_DIR%
 rmdir /S /Q %INSTALL_SRC_DIR%\NDigiDoc\bin
 rmdir /S /Q %INSTALL_SRC_DIR%\NDigiDoc\obj
 rmdir /S /Q %INSTALL_SRC_DIR%\NDigiDocUtil\bin
@@ -32,7 +38,10 @@ rmdir /S /Q %INSTALL_SRC_DIR%\packages
 
 echo "Build NDigiDoc"
 call %VC_VARS_CMD%
-cd %GIT_BUILD_DIR%
+cd %PROJECT_DIR%
+mkdir clrsecurity
+copy %INSTALL_DIR%\clrsecurity\*.* clrsecurity
+
 msbuild NDigiDoc.sln /p:Configuration=Install
 
 
--- packaging/package-ndigidoc.bat
+++ packaging/package-ndigidoc.bat
@@ -1,16 +1,22 @@
 rem --------------------------------------------
 rem package-ndigidoc.bat
+rem BEFORE USING THIS SCIPT 
+rem define in the environment:
+rem set BUILD_NUMBER=<some-build-number>
+rem set VC_DIR=<path-to-your-visual-studio>. Check that the provided location is correct.
+rem set PROJECT_DIR=<project-dir>. Where NDigiDoc.sln is
+rem set INSTALL_DIR=<output-directory>. Schould be c:\install\IdCard\ndigidoc
+rem because VC project outputs files there. This dir is used later for packaging.
 rem --------------------------------------------
 
 set build_number = %BUILD_NUMBER%
 set repository = %EXTERNAL_REPOSITORY%
-set INSTALL_DIR=c:\install
+echo INSTALL DIR: %INSTALL_DIR%
 SET INSTALL_BIN_DIR=%INSTALL_DIR%\IdCard\ndigidoc
 SET INSTALL_SRC_DIR=%INSTALL_DIR%\source\ndigidoc
-SET VC_DIR=C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC
+SET VC_DIR=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC
 SET VC_VARS_CMD="%VC_DIR%\vcvarsall.bat"
-SET GIT_SRC_DIR=c:\jenkins\workspace\nDigiDoc\idkaart\current\ndigidoc
-SET GIT_BUILD_DIR=c:\jenkins\workspace\nDigiDoc\label\Windows_trunk_VS2012\idkaart\current\ndigidoc
+echo PROJECT DIR: %PROJECT_DIR%
 set MSI_VERSION=%1
 set PREFIX=\install
 set sourceLocation=\install\source\ndigidoc
@@ -20,7 +26,7 @@ SET CANDLE_CMD="%WIX_DIR%\candle.exe"
 SET LIGHT_CMD="%WIX_DIR%\light.exe"
 
 echo "Packaging NDigiDoc.dll"
-cd %GIT_BUILD_DIR%
+cd %PROJECT_DIR%
 %HEAT_CMD% dir %sourceLocation% -cg Source -gg -scom -sreg -sfrag -srd -dr SourceFolder -var env.sourceLocation -out SourceFilesFragment.wxs
 %CANDLE_CMD% ndigidoc-dev.wxs SourceFilesFragment.wxs -v -ext WixIIsExtension
 %LIGHT_CMD% -out %INSTALL_BIN_DIR%\Eesti_ID_kaart-NET-teek-arendajale-%MSI_VERSION%.%build_number%.msi ndigidoc-dev.wixobj SourceFilesFragment.wixobj -v -ext WixIIsExtension
